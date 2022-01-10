ec68d5eff61f661ff65dda08c4bd1f97e7c27b76
chw_owo <gpdnjs2116@naver.com>
2022-01-10 19:36:36 +0900

    postingPage1

--- app.py
+++ app.py
@@ -4,15 +4,11 @@ app = Flask(__name__)
 ## 메인페이지
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('mainPage.html')
 
 @app.route('/postingPage')
 def post():
     return render_template('postingPage.html')
-
-@app.route('/testPage')
-def test():
-    return render_template('testPage.html')
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
